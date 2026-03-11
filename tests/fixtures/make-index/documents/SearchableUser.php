e36e10e95c7cdd05b36bf6b5144fca2fe256f616
Pauline Vos <pvos88@gmail.com>
2026-03-11 16:35:22 +0100

    Support adding search indexes to documents (#17)
    * Create test trait for skipping functional tests
    
    So that tests can be skipped based on feature support
    
    * Support adding search indexes to Documents

---
+++ tests/fixtures/make-index/documents/SearchableUser.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Attribute as ODM;
+
+#[ODM\Document]
+class SearchableUser
+{
+    #[ODM\Field]
+    public ?string $firstName = null;
+
+    #[ODM\Id]
+    public ?int $id = null;
+
+    #[ODM\Field]
+    public ?string $lastName = null;
+}
