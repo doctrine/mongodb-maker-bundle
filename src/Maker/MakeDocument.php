86cc959bf0b5679b9d809f0f177375280bcc84ac
Pauline Vos <pauline.vos@mongodb.com>
2026-03-17 14:40:40 +0100

    Handle Document relation compatibility
    `Reference*` relationships cannot target `EmbeddedDocument`s and
    vice-versa for standalone `Document`s. This commit adds checks for
    compatibility and offers to change the document type (if possible).

--- src/Maker/MakeDocument.php
+++ src/Maker/MakeDocument.php
@@ -8,8 +8,11 @@ use Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle;
 use Doctrine\Bundle\MongoDBMakerBundle\MongoDB\ClassSourceManipulator;
 use Doctrine\Bundle\MongoDBMakerBundle\MongoDB\DocumentClassGenerator;
 use Doctrine\Bundle\MongoDBMakerBundle\MongoDB\DocumentRelation;
+use Doctrine\Bundle\MongoDBMakerBundle\MongoDB\Exception\IncompatibleParentRelationException;
 use Doctrine\Bundle\MongoDBMakerBundle\MongoDB\MongoDBHelper;
 use Doctrine\Bundle\MongoDBMakerBundle\MongoDB\Validator;
+use Doctrine\ODM\MongoDB\Mapping\Attribute\Document;
+use Doctrine\ODM\MongoDB\Mapping\Attribute\EmbeddedDocument;
 use Doctrine\ODM\MongoDB\Types\Type;
 use Exception;
 use InvalidArgumentException;
@@ -182,6 +185,20 @@ final class MakeDocument extends AbstractMaker implements InputAwareMakerInterfa
                     $otherManipulator         = $this->createClassManipulator($otherManipulatorFilename);
                 }
 
+                try {
+                    $updateNewFieldTargetClass = $this->handleRelationCompatibility($newField, $io, $otherManipulator);
+                } catch (IncompatibleParentRelationException $e) {
+                    $errorMsg = $e->getMessage() . "\n";
+
+                    foreach ($e->parents as $parent) {
+                        $errorMsg .= "\n - " . $parent;
+                    }
+
+                    $io->error($errorMsg);
+                    $io->text('Skipping this field.');
+                    continue;
+                }
+
                 switch ($newField->getType()) {
                     case DocumentRelation::REFERENCE_ONE:
                         if ($newField->getOwningClass() === $documentClassDetails->getFullName()) {
@@ -241,7 +258,7 @@ final class MakeDocument extends AbstractMaker implements InputAwareMakerInterfa
                 }
 
                 // save the inverse side if it's being mapped
-                if ($newField->getMapInverseRelation()) {
+                if ($newField->getMapInverseRelation() || $updateNewFieldTargetClass) {
                     $fileManagerOperations[$otherManipulatorFilename] = $otherManipulator;
                 }
 
@@ -260,6 +277,53 @@ final class MakeDocument extends AbstractMaker implements InputAwareMakerInterfa
             'Next: Add more fields with the same command, or start using your document!',
             '',
         ]);
+    }
+
+    /** @throws IncompatibleParentRelationException */
+    private function handleRelationCompatibility(DocumentRelation $relation, ConsoleStyle $io, ClassSourceManipulator $manipulator): bool
+    {
+        // ToDo: handle self-referencing, as the class might not exist yet.
+        $type = $relation->getType();
+        $className = $relation->getTargetClassName();
+        assert(class_exists($className));
+
+        if (in_array($type, [DocumentRelation::EMBED_ONE, DocumentRelation::EMBED_MANY], true) && ! $this->mongoDBHelper->hasEmbeddedDocumentAttribute($className)) {
+            $io->warning('A document that is not marked as embedded can only be added as a `ReferenceOne` or `ReferenceMany` relation.');
+            if (! $io->confirm(sprintf('Would you like to make %s embedded?', $className), true)) {
+                return false;
+            }
+
+            $referencingParents = $this->mongoDBHelper->findReferencedBy($className);
+
+            if (! empty($referencingParents)) {
+                throw IncompatibleParentRelationException::forReferencingParents($className, ...$referencingParents);
+            }
+
+            $manipulator->removeAttributeFromClass(Document::class);
+            $manipulator->addAttributeToClass(EmbeddedDocument::class, []);
+
+            return true;
+        }
+
+        if (! $this->mongoDBHelper->hasEmbeddedDocumentAttribute($className)) {
+            return false;
+        }
+
+        $io->warning('An embedded document can only be added as an `EmbedOne` or `EmbedMany` relation.');
+        if (! $io->confirm(sprintf('Would you like to make %s a standalone document instead?', $className), true)) {
+            return false;
+        }
+
+        $embeddingParents = $this->mongoDBHelper->findEmbeddedBy($className);
+
+        if (! empty($embeddingParents)) {
+            throw IncompatibleParentRelationException::forEmbeddingParents($className, ...$embeddingParents);
+        }
+
+        $manipulator->addAttributeToClass(Document::class, []);
+        $manipulator->removeAttributeFromClass(EmbeddedDocument::class);
+
+        return true;
     }
 
     public function configureDependencies(DependencyBuilder $dependencies, ?InputInterface $input = null): void
